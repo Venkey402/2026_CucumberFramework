93c0428b3c274faf8fa42e54276cce3f42d216c7
venkat <venkey402@gmail.com>
2026-04-11 13:40:27 +0530

    mvn test "-Dcucumber.filter.tags=@ProfileSummaryUpdate"

--- src/test/java/com/naukri/TestRunner/TestRunner.java
+++ src/test/java/com/naukri/TestRunner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/com/naukri/Features",
         glue = "com/naukri/StepDefs",
-        tags = "@ProfileSummaryUpdate",
+//        tags = "@ProfileSummaryUpdate",
         monochrome = true,
         plugin = {"pretty", "html:target/Cucumber.html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
