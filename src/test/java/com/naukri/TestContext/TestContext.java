aeb695ac62ec8a4978b25f178842660cd7bd4387
venkat <venkey402@gmail.com>
2026-04-11 13:15:37 +0530

    added cross browser testing- chrome, edge

--- src/test/java/com/naukri/TestContext/TestContext.java
+++ src/test/java/com/naukri/TestContext/TestContext.java
@@ -2,15 +2,27 @@ package com.naukri.TestContext;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class TestContext {
 
     public WebDriver driver;
-
+    String browser;
     public TestContext()
     {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/Drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+        try {
+            browser = System.getProperty("browser");
+            if (browser.equalsIgnoreCase("chrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/Drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+            } else if (browser.equalsIgnoreCase("edge")) {
+                System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/src/test/resources/Drivers/msedgedriver.exe");
+                driver = new EdgeDriver();
+            }
+        }catch (Exception e){
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/Drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
     }
 
 
